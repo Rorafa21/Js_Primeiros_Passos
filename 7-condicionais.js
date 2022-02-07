c82539a24acd33f297b7dacf37ae4de715be4356
Rorafa21 <rodrigorafa71@gmail.com>
2022-02-07 18:01:11 -0300

    identação

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -1,8 +1,8 @@
 console.log("Trabalhando com condicionais")
-                        // [] <== conta como lista
+// [] <== conta como lista
 const listaDeDestinos = new Array(
-    "Salvador" ,
-    "São Paulo" ,
+    "Salvador",
+    "São Paulo",
     "Rio de Janeiro" ,
 )
 const idadeComprador = 15
@@ -11,20 +11,23 @@ const estaAcompanhada = true
 console.log("Destinos possiveis:")
 console.log(listaDeDestinos)
 
-if (idadeComprador >= 18){
+if (idadeComprador >= 18) {
+
     console.log("Comprador maior de idade. :D")
-    listaDeDestinos.splice(1,1) //removendo item
-}else if
-    (estaAcompanhada){
-        console.log("Comprador está acompanhado")
-        listaDeDestinos.splice(1,1)
-    }else{
-        console.log("Comprador não é maior de idade!!!")
-    }
-    
+    listaDeDestinos.splice(1, 1) //removendo item
+
+} else if (estaAcompanhada) {
+
+    console.log("Comprador está acompanhado")
+    listaDeDestinos.splice(1, 1)
+
+} else {
+    console.log("Comprador não é maior de idade!!!")
+}
 
 
-    
+
+
 
 
 console.log(listaDeDestinos)
