89b2bb07141f80f76955b48aac098a0b6083826a
Rorafa21 <rodrigorafa71@gmail.com>
2022-02-07 15:51:33 -0300

    conceito de lista simples

--- 6-listas.js
+++ 6-listas.js
@@ -1,2 +1,8 @@
 console.log("Trabalhando com listas")
+const salvador = `Salvador`
+const saoPaulo = `São Paulo`
+const rioDeJaneiro = `Rio de Janeiro`
+
+console.log("Destinos possiveis:")
+console.log(salvador, saoPaulo, rioDeJaneiro)
 
