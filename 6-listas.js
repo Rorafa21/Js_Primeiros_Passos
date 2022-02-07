6ee5e3f5cb90227a73326dd875d4ffb3ff449829
Rorafa21 <rodrigorafa71@gmail.com>
2022-02-07 17:33:08 -0300

    splice

--- 6-listas.js
+++ 6-listas.js
@@ -1,19 +1,21 @@
-console.log("Trabalhando com listas");
+console.log("Trabalhando com listas")
 // const salvador = `Salvador`;
 // const saoPaulo = `São Paulo`;
 // const rioDeJaneiro = `Rio de Janeiro`;
-
+                        // [] <== conta como lista
 const listaDeDestinos = new Array(
     "Salvador" ,
     "São Paulo" ,
     "Rio de Janeiro" ,
-    
-);
+)
 
 listaDeDestinos.push("Curitiba") // adicionando item na lista
 
-console.log("Destinos possiveis:");
+console.log("Destinos possiveis:")
 //console.log(salvador, saoPaulo, rioDeJaneiro);
 
 console.log(listaDeDestinos)
 
+listaDeDestinos.splice(1,1)
+console.log(listaDeDestinos)
+console.log(listaDeDestinos[1],"/", listaDeDestinos[0])
